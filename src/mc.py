264b47c68b892709be9d7e813f46187b8f711f34
ShunkiKyoya <kyoya.sus4@gmail.com>
2021-03-22 01:01:11 +0900

    modify mc: new definition

--- src/mc.py
+++ src/mc.py
@@ -7,23 +7,44 @@ from scipy.special import logsumexp
 from .utils import _log_prob
 
 
-def _mc(*, pi, log_prob, weights):
-    """Calculate mixture complexity from log_prob.
+def _mc(*, prob_latent, weights):
+    """Calculate mixture complexity from prob_latent.
 
     Args:
-        log_prob (np.ndarray, shape=(N, K)): log probability matrix.
-        pi (np.ndarray, shape=(K,)): mixture proportions.
-        weights (np.ndarray): data weights.
+        prob_latent (np.ndarray, shape=(N, K)): latent probabilities.
+        weights (np.ndarray, shape=(N,)): data weights.
     Returns:
         float: mixture complexity.
     """
+    N = len(prob_latent)
+    if weights is None:
+        weights = np.ones(N)
+
+    rho = np.dot(weights, prob_latent) / np.sum(weights)
+    H_Z = - np.dot(rho, np.log(rho + 1e-50))
+    H_ZbarX = (
+        - np.sum(np.dot(weights, prob_latent * np.log(prob_latent + 1e-50)))
+        / np.sum(weights)
+    )
+    return H_Z - H_ZbarX
+
+
+def _prob_latent(*, X, pi, means, covariances):
+    """Calculate latent probabilities.
+
+    Args:
+        X (np.ndarray, shape=(N, D)): data.
+        pi (np.ndarray, shape=(K,)): mixture proportions.
+        means (np.ndarray, shape=(K, D)): mean vectors.
+        covariances (np.ndarray, shape=(K, D, D)): covariance matrices.
+        weights (np.ndarray, shape=(N,)): data weights.
+    Returns:
+        np.ndarray: latent probabilities (shape = (N, K)).
+    """
+    log_prob = _log_prob(X=X, means=means, covariances=covariances)
     log_pi_prob = np.log(pi + 1e-50) + log_prob
     log_pi_prob_norm = logsumexp(log_pi_prob, axis=1).reshape([-1, 1])
-
-    term1 = log_pi_prob - log_pi_prob_norm
-    term2 = log_prob - log_pi_prob_norm
-
-    return sum(np.dot(weights, np.exp(term1) * term2)) / sum(weights)
+    return np.exp(log_pi_prob - log_pi_prob_norm)
 
 
 def mc(*, X, pi, means, covariances, weights=[]):
@@ -41,9 +62,9 @@ def mc(*, X, pi, means, covariances, weights=[]):
     N = len(X)
     if len(weights) == 0:
         weights = np.ones(N)
-    log_prob = _log_prob(X=X, means=means, covariances=covariances)
+    prob_latent = _prob_latent(X=X, pi=pi, means=means, covariances=covariances)
 
-    return _mc(pi=pi, log_prob=log_prob, weights=weights)
+    return _mc(prob_latent=prob_latent, weights=weights)
 
 
 def _mc_decomp(log_prob, pi, partition):
@@ -71,14 +92,17 @@ def _mc_decomp(log_prob, pi, partition):
     log_w = log_rho_prob_h - log_rho_prob_h_norm
     w = np.exp(log_w)
 
-    MC_interaction = _mc(log_prob=log_prob_h, pi=rho, weights=np.ones(N))
+    MC_interaction = _mc(prob_latent=w, weights=np.ones(N))
 
     W = np.zeros(L)
 
     MC_local = np.zeros(L)
     for l in range(L):
         W[l] = sum(w[:, l]) / N
-        MC_local[l] = _mc(log_prob=log_prob, pi=phi[:, l], weights=w[:, l])
+        log_phi_prob = np.log(phi[:, l] + 1e-50) + log_prob
+        log_phi_prob_norm = logsumexp(log_phi_prob, axis=1).reshape([-1, 1])
+
+        MC_local[l] = _mc(prob_latent=np.exp(log_phi_prob - log_phi_prob_norm), weights=w[:, l])
 
     return MC_interaction, W, MC_local
 
